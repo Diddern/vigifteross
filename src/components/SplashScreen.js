d8b75b23777a1b88eaf69e4b168770fc0a7ff0e6
Didrik Sæther <didrik.saether@gjensidige.no>
2024-04-15 16:29:18 +0200

    Refactor of App and fixing styling of text and sizing of map

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 
 const SplashScreen = ({ imageUrl, children }) => {
+
+    const calculateCountdown = (weddingDate) => {
+        const currentDate = new Date();
+        const timeDifference = weddingDate - currentDate;
+
+        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+        return `${days} dager ${hours} timer ${minutes} minutter og ${seconds} sekunder`;
+    };
+
+
+    const weddingDate = useMemo(() => new Date('2025-06-21T00:00:00'), []);
+    const [countdown, setCountdown] = useState(calculateCountdown(weddingDate));
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCountdown(calculateCountdown(weddingDate));
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [weddingDate]);
+
+    const scrollToForm = () => {
+        document.getElementById('rsvpForm').scrollIntoView({ behavior: 'smooth' });
+    };
+
+
     return (
         <div id="splash">
             <div className="page-header-image" style={{ backgroundImage: `url(${imageUrl})` }}></div>
-            <div>{children}</div>
+            <div className="splash-content">
+                <h1>Helga og Didrik gifter seg</h1>
+                <h3>21.06.2025 kl 12:00</h3>
+                <h2 id="countdown"
+                    className="d-flex flex-column align-items-center justify-content-center text-white">Det er
+                    om {countdown}</h2>
+                <button className="rsvp-btn" onClick={scrollToForm}>RSVP</button>
+            </div>
         </div>
     );
 };
