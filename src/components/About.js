eb5c7acba62c21e0f2548c7d8fd5644eb394bd34
Didrik Sæther <didrik.saether@gjensidige.no>
2024-04-19 18:36:30 +0200

    Lagt til om-tekst

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,10 @@ const About = () => {
             <div className="about-content">
                 <h2>Om brudeparet</h2>
                 <p>
-                    En søt historie om brudeparet, og hvordan de møttes kommer her.
+                    Helga og Didrik møttes en regnværsdag i Bergen 2022 på fest på Støletorget.
+                    Etter langvarig vennskap, utallige dårlige vitser og intense samtaler om Marvel og whiskey flyttet Helga inn i leiligheten til Didrik i 2017.
+                    <br/>
+                    Etter åtte år, en leilighet i Oslo, et hus i Asker, reiser, skiturer, seilturer, oppussing og hytteturer skal de gifte seg i Asker kirke og feire med sine nærmeste venner og familie.
                 </p>
             </div>
         </div>
